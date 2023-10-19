4a97393008a38e5f2b3bf353afa925e670dfcbad
ShamilBiktimirov <biktimirovshamil@gmail.com>
2023-10-19 15:36:30 +0400

    added walker constellation patterns

---
+++ orbitDesign/walkerConstellation.m
@@ -0,0 +1,26 @@
+function oes = walkerConstellation(T, P, f, a, inc, walkerType)
+
+    oes = zeros(T, 6);
+    q = T/P;
+    e = 0;
+    w = 0;
+
+    if strcmpi(walkerType, 'star')
+        delta_RAAN = 180/P;
+    elseif strcmpi(walkerType ,'delta')
+        delta_RAAN = 360/P;
+    else
+        error('incorrect walker type');
+    end
+
+    i = 0;
+    for k = 1:P
+        for j = 1:q
+            i = i + 1;
+            RAAN = mod((k - 1)*delta_RAAN, 360);
+            M = mod(360*(j - 1)/q + 360*f*(k - 1)/T, 360);
+            oes(i,:) = [a; e; inc; RAAN; w; M];
+        end
+    end
+
+end
