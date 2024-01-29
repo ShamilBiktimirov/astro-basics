18edadc20e83cc8c4838e474a5412308c890635f
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-01-29 13:59:57 +0400

    updated fibonacci lattice spherical coordinates calculation function

--- coverageGeometry/calcUniformGridLatLon.m
+++ coverageGeometry/calcUniformGridLatLon.m
@@ -1,15 +1,26 @@
-function [lat, lon] = calcUniformGridLatLon(gridResolution)
+function [latAarray, lonArray] = calcUniformGridLatLon(gridResolution)
+
+    % Algorithm generates lat, lon for spherical Fibonnaci grid/lattice
+    % The more details available at https://www.overleaf.com/read/hsjtzhnncmfw#71ff51
+
+    % Input: grid resolution
+    % - gridResolution, m
+
+    % Output: points spherical coordinates
+    % - latArray, rad
+    % - lonArray, rad
 
     epsilon = 0.5; % grid building tuning parameter to avoid singularities at the poles
 
-    nNodes  = 16 * Consts.rEarth^2 / gridResolution^2;
+    nNodes  = round(16 * Consts.rEarth^2 / gridResolution^2);
 
     nodesIdxArray = 1 : nNodes;
 
-    yArray = (nodesIdxArray + epsilon) ./ (nNodes - 1 + 2 * epsilon);
-    xArray = mod(nodesIdxArray / Consts.goldenRatio, 1);
+    % square lattice coordinates
+    xArray = mod((nodesIdxArray - 1) / Consts.goldenRatio, 1);
+    yArray = (nodesIdxArray - 1 + epsilon) ./ (nNodes + 2 * epsilon);
 
-    lat = acos(1 - 2 * xArray);
-    lon = 2 * pi * yArray;
+    latAarray = pi / 2 - acos(1 - 2 * xArray);
+    lonArray = 2 * pi * yArray;
 
 end
