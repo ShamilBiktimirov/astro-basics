ef41d9aee2ee538e4135fc1c17563b83277c2527
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-01-31 13:06:51 +0400

    added j2 analytical propagator with ECEF coordinates output

--- coverageGeometry/calcCoveredPoints.m
+++ coverageGeometry/calcCoveredPoints.m
@@ -1,22 +1,23 @@
-function coveredPoints = calcCoveredPoints(rEcef, earthGrid, betaAngle)
+function coveredPoints = calcCoveredPoints(rSat, rPOI, betaAngle)
 
     % Function finds a set of nodes on Earth located within area bounded by betaAngles
     % ToDo: provide a description document in overleaf
 
     % Input:
-    % rEcef [3, n]
-    % earthGrid [3, m]
+    % - rSat [3, n], an array of satellite position vectors
+    % - rPOI [3, m], an array of position vectors for points of interest (POI)
+    % - betaAngle, rad, included angle for circular FOV coverage check
 
     % Output:
-    % coveredPoints [m, 1] - logical vectoral of covered points
+    % coveredPoints [m, 1] - logical vector of covered points
 
-    ePoiArray  = earthGrid;
+    ePoiArray  = rPOI;
 
-    eSat = rEcef ./ repmat(vecnorm(rEcef, 2, 1), [3, 1]);
+    eSat = rSat ./ repmat(vecnorm(rSat, 2, 1), [3, 1]);
     eSat = reshape(eSat, [size(eSat, 1), 1, size(eSat, 2)]);
 
     ePoiArray3D = repmat(ePoiArray, [1, 1, size(eSat, 3)]);
-    eSatArray3D = repmat(eSat, [1, size(earthGrid, 2), 1]);
+    eSatArray3D = repmat(eSat, [1, size(rPOI, 2), 1]);
 
     coveredPointsMatrix = squeeze(dot(ePoiArray3D, eSatArray3D, 1)) >= cos(betaAngle);
 
---
+++ rhs/propJ2AnalyticalCircECEF.m
@@ -0,0 +1,100 @@
+function rv = propJ2AnalyticalCircECEF(oe, JD)
+
+    % Input
+    % mean orbital elements, oe = [a; e; i; RAAN; AOP; MA]
+    % time vector, t 
+
+    % Output
+    % rv(t)
+
+    % so far for one satellite and for circular orbits
+
+    a0    = oe(1);
+    e0    = oe(2);
+    i0    = oe(3);
+    RAAN0 = oe(4);
+    AOP0  = oe(5);
+    MA0   = oe(6);
+
+    c = 3 / 2 * sqrt(Consts.muEarth) * Consts.rEarthEquatorial ^ 2 * Consts.earthJ2;
+
+    RAANprecessionRate = - c / ((1 - e0^2)^2 * a0^(7/2)) * cos(i0);
+    AOPprecessionRate  = - c / ((1 - e0^2)^2 * a0^(7/2)) * (5/2 * sin(i0)^2 - 2);
+    MApressionRate     = - c / ((1 - e0^2)^2 * a0^(7/2)) * sqrt(1 - e0^2) * (3/2 * sin(i0)^2 - 1);
+
+    n = sqrt(Consts.muEarth/ a0^3);
+
+    t = (JD - JD(1)) * Consts.day2sec;
+
+    GST = JD2GMST(JD) * pi / 180;
+
+    argOfLat = AOP0 + AOPprecessionRate * t + MA0 + MApressionRate*t + n * t;
+    RAAN     = RAAN0 + RAANprecessionRate * t;
+
+
+    M1 = zeros(3, 3, length(t));
+    M3 = zeros(3, 3, length(t));
+    EarthDCM = zeros(3, 3, length(t));
+
+    % first rotation on RAAN angle
+    M1(:, 1, :) = [cos(RAAN); ...
+                   sin(RAAN); ...
+                   zeros(1, length(t))];
+
+    M1(:, 2, :) = [-sin(RAAN); ...
+                   cos(RAAN); ...
+                   zeros(1, length(t))];
+
+    M1(:, 3, :) = [zeros(1, length(t)); ...
+                   zeros(1, length(t)); ...
+                   ones(1, length(t))];
+
+    % second rotation along x axis on inclination angle
+    M2 = [[1, 0, 0]; ...
+          [0, cos(i0), -sin(i0)]; ...
+          [0, sin(i0), cos(i0)]];
+
+    M2 = repmat(M2, 1, 1, length(t));
+
+    % third rotation around z axis on agr of lat angle
+    M3(:, 1, :) = [cos(argOfLat); ...
+                   sin(argOfLat); ...
+                   zeros(1, length(t))];
+
+    M3(:, 2, :) = [sin(argOfLat); ...
+                   cos(argOfLat); ...
+                   zeros(1, length(t))];
+
+    M3(:, 3, :) = [zeros(1, length(t)); ...
+                   zeros(1, length(t)); ...
+                   ones(1, length(t))];
+
+
+    % Earth attitude DCM
+    EarthDCM(:, 1, :) = [cos(GST); ...
+                         -sin(GST); ...
+                         zeros(1, length(t))];
+
+    EarthDCM(:, 2, :) = [sin(GST); ...
+                         cos(GST); ...
+                         zeros(1, length(t))];
+
+    EarthDCM(:, 3, :) = [zeros(1, length(t)); ...
+                         zeros(1, length(t)); ...
+                         ones(1, length(t))];
+
+    M1M2 = pagemtimes(M1, M2);
+    orb2ECIdcm = pagemtimes(M1M2, M3);
+
+    rECI = squeeze(pagemtimes(orb2ECIdcm, repmat([a0; 0; 0], 1, 1, length(t))));
+    vECI = squeeze(pagemtimes(orb2ECIdcm, repmat([0; sqrt(Consts.muEarth / a0); 0], 1, 1, length(t))));
+
+
+    rECEF = squeeze(pagemtimes(EarthDCM, reshape(rECI, [3, 1, size(rECI, 2)])));
+    % apply transport theorem to find velocity in rotating frame
+    vECEF = squeeze(pagemtimes(EarthDCM, reshape(vECI - cross(repmat(Consts.wEarth, 1, length(t)), rECI), [3, 1, size(rECI, 2)])));
+
+
+    rv = [rECEF; vECEF];
+
+end
