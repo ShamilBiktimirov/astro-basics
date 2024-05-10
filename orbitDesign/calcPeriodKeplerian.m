4a4e8c7af33a6fd73560f357732c7c9aa6500a66
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-05-10 18:38:43 +0400

    added precession rates calculation function and example verifying its correctness

---
+++ orbitDesign/calcPeriodKeplerian.m
@@ -0,0 +1,18 @@
+function period = calcPeriodKeplerian(a)
+
+    % Function calculates Keplerian orbital period for a given semi-major axis
+    % Note: The orbital parameter should be mean if working with J2 perturbed dynamics for proper period estimate
+
+    % Reference:
+    % The formulas for the drifts are taken from D.A. Vallado Fundamentals of Astrodynamics
+    % page 869, eq. 11-22
+
+    % Input:
+    % a [m], semi-major axis
+
+    % Output:
+    % period [s], Keplerian orbit period
+
+    period = 2 * pi * sqrt(a^3 / Consts.muEarth);
+
+end
