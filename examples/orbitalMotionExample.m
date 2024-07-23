ba3c13eaaf07078513ad618a249d8cee7d52a1fa
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-07-23 10:41:44 +0400

    updated functions name after old mission design functions review

--- examples/orbitalMotionExample.m
+++ examples/orbitalMotionExample.m
@@ -10,9 +10,9 @@ epochJD = juliandate(epochGD);
 oe             = zeros(6, 1);
 oe(1)          = Consts.rEarth + 350e3; % orbit sma, m
 oe(2)          = 0;                     % ecc
-oe(3)          = get_SSO_inclination(oe(1), oe(2)); % sso orbit inclination, rad
-epochGD = datetime(epochJD, 'convertfrom','juliandate');
-oe(4)          = get_RAAN_for_terminator_orbit(epochGD); % RAAN for terminator oriented orbit
+oe(3)          = calcSsoOrbitInclination(oe(1), oe(2)); % sso orbit inclination, rad
+epochGD        = datetime(epochJD, 'convertfrom','juliandate');
+oe(4)          = calcRaanDuskDawn(epochGD); % RAAN for terminator oriented orbit
 oe(5)          = 0; % AOP, deg
 oe(6)          = 0; % M, deg - actually argument of latitude
 
@@ -26,3 +26,12 @@ tspan = 1 : 2 * pi * sqrt(oe(1)^3 / Consts.muEarth);
 options = odeset('RelTol',1e-8,'AbsTol',1e-8);
 [t_vec, rvECI] = ode45(@(t, rv) rhsFormationInertial(t, rv, [], spacecraft), tspan, rvECIBaseOrbit, options);
 rvECI = rvECI';
+
+figure;
+plot3(rvECI(1, :), rvECI(2, :), rvECI(3, :), 'k', 'LineWidth', 2);
+
+title('satellite position r(t)');
+xlabel('x, m');
+ylabel('y, m');
+zlabel('z, m');
+grid on;
--- orbitDesign/calcFundamentalGroundTrackInterval.m
+++ orbitDesign/calcFundamentalGroundTrackInterval.m
@@ -3,7 +3,7 @@ function sF = calcFundamentalGroundTrackInterval(oe)
     % orbit is assumed as circular
 
     [raanDot, ~, ~] = calcSecularPrecessionRates(oe(1, :), zeros(1, size(oe, 2)), oe(3, :));
-    periodNodal     = calcDraconicPeriod([oe(1, :); zeros(1, size(oe, 2)); oe(3, :)]);
+    periodNodal     = calcPeriodNodal([oe(1, :); zeros(1, size(oe, 2)); oe(3, :)]);
 
     sF = (Consts.omegaEarth - raanDot) .* periodNodal; % fundamental interval, radians
 
