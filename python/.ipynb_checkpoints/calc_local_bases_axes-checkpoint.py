1d6b60d8c55fbc8cc8b1e969e47963640fd27a2f
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-10-31 13:02:13 +0400

    working pipeline, needs debugging

--- python/.ipynb_checkpoints/calc_local_bases_axes-checkpoint.py
+++ python/.ipynb_checkpoints/calc_local_bases_axes-checkpoint.py
@@ -10,15 +10,36 @@ def calc_local_bases_axes(latitude, longitude):
                    
     return A
 
-def calc_local_azimuth_and_declination(position_vector):
+def calc_local_azimuth_and_elevation(r_obs_eci, r_sat_eci):
+    
+    longitude, latitude = calc_right_asc_and_declination(r_obs_eci) # because it simply works a cart to sph
+    A_eci_to_local = calc_local_bases_axes(latitude, longitude)
+    
+    r_rel_local = np.matmul(A_eci_to_local, r_sat_eci - r_obs_eci)
+    
+    x = r_rel_local[0]
+    y = r_rel_local[1]
+    z = r_rel_local[2]
+    
+    # see to the f igure for more details on the reference frame and angles
+    az, elev = calc_right_asc_and_declination(r_rel_local)    
+    return az - np.pi/2, elev
+
+def calc_right_asc_and_declination(position_vector):
     
     x = position_vector[0]
     y = position_vector[1]
     z = position_vector[2]
     
-    azimuth = np.arctan2(-x, y)
-    declination = np.arctan2(z, x**2 + y**2)
+    sin_decl = z / np.linalg.norm(position_vector)
+    cos_decl = (x**2 + y**2)**(1/2) / np.linalg.norm(position_vector)
     
-    return azimuth, declination
+    declination = np.arctan2(sin_decl, cos_decl)
     
+    sin_ra = y / (x**2 + y**2)**(1/2)
+    cos_ra = x / (x**2 + y**2)**(1/2)
+    right_ascension = np.arctan2(sin_ra, cos_ra)
+    
+    return right_ascension, declination
+
     
