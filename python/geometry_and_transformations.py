cf08c3147c78e58f46fc5f9a8ba0df81d99ab0b3
ShamilBiktimirov <biktimirovshamil@gmail.com>
2024-11-12 18:31:41 +0400

    notebook revision

--- python/geometry_and_transformations.py
+++ python/geometry_and_transformations.py
@@ -1,5 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def cart_to_sph(position_vector):
+    # TODO add rho coordinate
+    phi_sph = np.arcsin(position_vector[2] / np.linalg.norm(position_vector))
+
+    lambda_sph = np.arctan2(position_vector[1], position_vector[0])
+
+    return phi_sph, lambda_sph
+
 def calc_local_bases_axes(latitude, longitude):
+
+    # find a matrix of unit vectors of local reference frame
 
     ex = np.vstack([-np.sin(longitude), np.cos(longitude), 0])
     ey = np.vstack([-np.sin(latitude) * np.cos(longitude), -np.sin(latitude) * np.sin(longitude), np.cos(latitude)])
@@ -10,9 +22,12 @@ def calc_local_bases_axes(latitude, longitude):
                    
     return A
 
-def calc_local_azimuth_and_elevation(r_obs_eci, r_sat_eci):
+def calc_azimuth_and_elevation(r_obs_eci, r_sat_eci):
+
+    # elevation is angle with reference plane - local horizon
+    # azimuth is angle from North in clock-wise direction
     
-    longitude, latitude = calc_right_asc_and_declination(r_obs_eci) # because it simply works a cart to sph
+    latitude, longitude = cart_to_sph(r_obs_eci)
     A_eci_to_local = calc_local_bases_axes(latitude, longitude)
     
     r_rel_local = np.matmul(A_eci_to_local, r_sat_eci - r_obs_eci)
@@ -21,57 +36,23 @@ def calc_local_azimuth_and_elevation(r_obs_eci, r_sat_eci):
     y = r_rel_local[1]
     z = r_rel_local[2]
     
-    # see to the f igure for more details on the reference frame and angles
-    az, elev = calc_right_asc_and_declination(r_rel_local)
-    az = az - np.pi/2 # because y-axis is reference direction and it points to the north
+    sin_elev = z / np.linalg.norm(r_rel_local)
+    cos_elev = (x**2 + y**2)**(1/2) / np.linalg.norm(r_rel_local)
     
-    if az > 0 and az > np.pi:
-        az = 2 * np.pi - az
-    elif az < 0 and np.absolute(az) > np.pi:
-        az = 2 * np.pi + az
-        
+    elev = np.arctan2(sin_elev, cos_elev)
+    az = np.arctan2(x, y)
+    
     return az, elev
 
-def calc_right_asc_and_declination(position_vector):
+def calc_ra_and_dec_topocentric(r_sat_eci, r_obs_eci):
 
-    # Calculates geocentric celestial coordinates,
-    # Adopted from Algorithm 25, D. Vallado, Fundamentals of astrodynamics and applications
-    
-    x = position_vector[0]
-    y = position_vector[1]
-    z = position_vector[2]
-    
-    sin_decl = z / np.linalg.norm(position_vector)
-    cos_decl = (x**2 + y**2)**(1/2) / np.linalg.norm(position_vector)
-    
-    declination = np.arctan2(sin_decl, cos_decl)
-    
-    sin_ra = y / (x**2 + y**2)**(1/2)
-    cos_ra = x / (x**2 + y**2)**(1/2)
-    right_ascension = np.arctan2(sin_ra, cos_ra) # returns values from -pi to pi
+    # Calculates topocentric celestial coordinates
 
-    return right_ascension, declination
+    r_obs2sat = r_sat_eci - r_obs_eci
 
+    right_ascension_topo, declination_topo = cart_to_sph(r_obs2sat)
 
-def calc_right_asc_and_declination_Topo(sat_eci, obs_eci):
-
-    # Calculates geocentric celestial coordinates,
-    # Adopted from Algorithm 25, D. Vallado, Fundamentals of astrodynamics and applications
-
-    position_vector = sat_eci - obs_eci
-    x = position_vector[0]
-    y = position_vector[1]
-    z = position_vector[2]
-    
-    declination = np.arcsin(z / np.linalg.norm(position_vector))
-    
-    sin_ra = y / (x**2 + y**2)**(1/2)
-    cos_ra = x / (x**2 + y**2)**(1/2)
-    right_ascension = np.arctan2(sin_ra, cos_ra) # returns values from -pi to pi
-
-    return right_ascension, declination
-
-
+    return right_ascension_topo, declination_topo
 
 def calc_geocentric_radius_for_ellipsoid(lat):
     from consts import Consts
@@ -83,3 +64,43 @@ def calc_geocentric_radius_for_ellipsoid(lat):
     r = Consts.rEarthEquatorial * (nom / denom)**(1/2)
     
     return r
+
+def degrees_to_dms(deg_value):
+    # Extract degrees
+    degrees = int(deg_value)
+    # Calculate minutes from the decimal part
+    minutes = int((deg_value - degrees) * 60)
+    # Calculate seconds from the remaining decimal part
+    seconds = (deg_value - degrees - minutes / 60) * 3600
+    return [degrees, minutes, seconds]
+
+def degrees_to_hms(deg_value):
+    if deg_value < 0: 
+        deg_value = 360 + deg_value  
+    # Convert degrees to hours
+    hours = int(deg_value / 15)
+    # Calculate minutes from the remaining decimal part
+    minutes = int((deg_value / 15 - hours) * 60)
+    # Calculate seconds from the remaining decimal part
+    seconds = ((deg_value / 15 - hours) * 60 - minutes) * 60
+    return [hours, minutes, seconds]
+
+def plot_sky_s(altaz_coordinates, epochs):
+    
+    # Set up the polar plot
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection='polar')
+    ax.set_rlim([90, 0])
+    ax.set_theta_zero_location('N')
+    ticks = [0, np.deg2rad(45), np.deg2rad(90), np.deg2rad(135), np.deg2rad(180), np.deg2rad(225), np.deg2rad(270), np.deg2rad(315)]
+    labels = ['N, 0°', 'NE, 45°', 'E, 90°', 'SE, 135°', 'S, 180°', 'SW, 225°', 'W, 270°', 'NW, 315°']
+    plt.xticks(ticks, labels)
+    ax.set_theta_direction(1)
+    
+    ax.plot(altaz_coordinates[:,0] * np.pi / 180, altaz_coordinates[:,1], 'bo--')
+
+    for point_idx in range(np.size(epochs)):
+        text = epochs[point_idx].strftime('%H:%M')
+        ax.text(altaz_coordinates[point_idx,0] * np.pi / 180, altaz_coordinates[point_idx,1], text, ha='right', va='bottom')
+
+    plt.show()
