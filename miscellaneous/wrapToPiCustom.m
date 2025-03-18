4427cc41184be89dc339db3b40502e7b4747fc9c
ShamilBiktimirov <biktimirovshamil@gmail.com>
2025-03-18 20:11:00 +0400

    updated function so it work for vectorized case

--- miscellaneous/wrapToPiCustom.m
+++ miscellaneous/wrapToPiCustom.m
@@ -1,13 +1,19 @@
 function angleOut = wrapToPiCustom(angleIn)
 
-    if abs(angleIn) > pi
-        if angleIn < 0
-            angleOut = 2 * pi + angleIn;
-        elseif angleIn > 0
-            angleOut = -2 * pi + angleIn;
-        end
-    else
-        angleOut = angleIn;
+    angleOut = [];
+    
+    for idx = 1:length(angleIn)
+
+            if abs(angleIn(idx)) > pi
+                if angleIn(idx) < 0
+                    angleOut(idx) = 2 * pi + angleIn(idx);
+                elseif angleIn > 0
+                    angleOut(idx) = -2 * pi + angleIn(idx);
+                end
+            else
+                angleOut(idx) = angleIn(idx);
+            end
+
     end
 
 end
