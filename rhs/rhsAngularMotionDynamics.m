e89145426f924765bdc2c8297bf8642705301536
FatimaAlnaqbi1998 <fatima.alnaqbi@tii.ae>
2025-07-25 10:14:51 +0400

    update qDot rhs equation

--- rhs/rhsAngularMotionDynamics.m
+++ rhs/rhsAngularMotionDynamics.m
@@ -1,19 +1,25 @@
-function Xdot = rhsAngularMotionDynamics(t, X, J, M)
+function qwPrime = rhsAngularMotionDynamics(t, X, J, M)
 
     % X = [q; omega] - angular dynamics state
     % J - inertia tensor
     % M - external torque
 
-    q = X(1:4);
-    omega = X(5:7);
+    qw = X;
+
+    intertiaTensorInverse =  inv(J);
+
+    unitQuaternion = qw(1:4) / norm(qw(1:4)); % normalize quaternion
 
     if isempty(M)
         M = [0; 0; 0]; % no external torques are applied
-    end
+    end 
 
-    qDot = 1 / 2 * quaternionMultiply([0; omega], q); % Poisson's equation describing kinematics
-    omegaDot = inv(J) * (M - cross(omega, J * omega));% Euler's equation describing dynamics
+    quaternionDot  = 1 / 2 * quatmultiply(unitQuaternion', [0; qw(5:7)]'); % Poisson equation
 
-    Xdot = [qDot; omegaDot];
+    angularVelocityDot = -intertiaTensorInverse * cross(qw(5:7), J * qw(5:7)) + ...
+                         intertiaTensorInverse * M; % Euler equation
+
+    qwPrime = [quaternionDot'; angularVelocityDot];
+
 
 end
